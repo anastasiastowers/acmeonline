d5e6a8e9b05af29082a2d97a46777802dfac433a
Anastasia Stowers <anastasia.k.stowers@gmail.com>
2018-08-02 16:49:35 -0500

    Setup devise as part of Storefront engine.

---
+++ engines/storefront/app/controllers/storefront/customers_controller.rb
@@ -0,0 +1,4 @@
+module Storefront
+  class CustomersController < ApplicationController
+  end
+end
--- engines/storefront/config/routes.rb
+++ engines/storefront/config/routes.rb
@@ -1,4 +1,16 @@
 Storefront::Engine.routes.draw do
-  devise_for :customers, class_name: "Storefront::Customer"
   root to: 'home#index'
+
+  devise_for :customers, class_name: 'Storefront::Customer', module: :devise, skip: [:sessions, :registrations]
+  as :customer do
+    get    'login', to: 'customers/sessions#new', as: :new_customer_session
+    post   'signin', to: 'customers/sessions#create', as: :customer_session
+    match  'signout', to: 'customers/sessions#destroy', as: :destroy_customer_session, via: Devise.mappings[:customer].sign_out_via
+
+    get    'register' => 'customers/registrations#new', as: :new_customer_registration
+    post   'register' => 'customers/registrations#create', as: :customer_registration
+    get    'confirmation_sent'  => 'customers/registrations#confirmation', as: :confirmation_sent
+    put    'signup'  => 'customers/registrations#update', as: :update_customer_registration
+    get    'account' => 'customers/registrations#edit',   as: :edit_customer_registration
+  end
 end
