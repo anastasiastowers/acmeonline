d521c1ef0f88c6eb047386d6eb5a7f1c3a5f2452
Anastasia Stowers <anastasia.k.stowers@gmail.com>
2018-08-06 08:56:14 -0500

    Remove commented code.

--- engines/payments/app/models/payments/api_key.rb
+++ engines/payments/app/models/payments/api_key.rb
@@ -1,15 +1,5 @@
 module Payments
   class ApiKey < ApplicationRecord
     has_secure_token :token
-
-    # after_create generate_token
-    # attr_accessor :name, :token
-
-    private
-
-    # def generate_token
-    #   token = SecureRandom.urlsafe_base64
-    #   self.update(token: token)
-    # end
   end
 end
