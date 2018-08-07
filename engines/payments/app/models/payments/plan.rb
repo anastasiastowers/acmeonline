0a4cd07964bd48436a2de206bae57be335db7788
Anastasia Stowers <anastasia.k.stowers@gmail.com>
2018-08-07 12:20:05 -0500

    Fix foreign key reference.

--- engines/payments/app/models/payments/plan.rb
+++ engines/payments/app/models/payments/plan.rb
@@ -1,6 +1,6 @@
 module Payments
   class Plan < ApplicationRecord
-    has_many :payments_subscriptions, class_name: 'Payments::Subscription'
+    has_many :payments_subscriptions, class_name: 'Payments::Subscription', foreign_key: :payments_plan_id
 
     validates :name, presence: true
     validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
--- engines/payments/app/models/payments/subscriber.rb
+++ engines/payments/app/models/payments/subscriber.rb
@@ -1,7 +1,18 @@
 module Payments
   class Subscriber < ApplicationRecord
-    has_many :subscriptions, class_name: 'Payments::Subscription'
+    has_many :subscriptions, class_name: 'Payments::Subscription', foreign_key: :payments_subscriber_id
 
     validates :customer_id, presence: true, numericality: { only_integer: true }
+
+    def package_details
+      subscriptions = payments_subscriptions
+
+      {
+          subscriber: attributes,
+          subscriptions: {
+
+          }
+      }
+    end
   end
 end
