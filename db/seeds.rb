37edcbde0753fb7f54f37425fd9d1578c79f88af
Anastasia Stowers <anastasia.k.stowers@gmail.com>
2018-08-07 13:32:31 -0500

    Add box Plans to seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Add Bronze Box Plan
+Payments::Plan.create(name: 'Bronze Box', price: '19.99')
+
+# Add Silver Box Plan
+Payments::Plan.create(name: 'Silver Box', price: '49.00')
+
+# Add Gold Box Plan
+Payments::Plan.create(name: 'Gold Box', price: '99.00')
